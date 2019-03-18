7e7ad31c6729966554234ceb122bef44dae320e4
saf0911 <saf0911@gmail.com>
2019-03-18 11:02:08 -0500

    added drop menu

---
+++ pages/about.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Link from 'next/link'
+import Head from '../components/head'
+import Header from '../components/headerTrial'
+
+
+
+const About = () => (
+  <div className='page'>
+    <Head title="ChickenRanch" />
+    <Header />
+
+  </div>
+)
+
+export default About
